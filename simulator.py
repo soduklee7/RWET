894a25d8af63e73ee8e248581f5a8459215e12e9
soduklee7 <62019112+soduklee7@users.noreply.github.com>
2024-10-26 17:35:00 -0400

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -20,9 +20,6 @@ from first_order_filter import first_order_filter
 from first_order_filter import MinMaxSlewRateFilter
 # from first_order_filter import delay_filter
 
-v_slew_filter = MinMaxSlewRateFilter(-2, 2)
-alpha_lpf = 0.25
-
 delta_t = 0.01
 vehicle_mass = 3000 * 0.453592  # in kg
 
@@ -67,10 +64,6 @@ front_wheels = FrontWheels(tire_radius)
 # v_cyc = np.array(mat['v_cyc'])
 # t_cyc = np.array(mat['t_cyc'])
 
-v_out_slew = []
-v_delay = []
-
-v_out = []
 power_req_out = []
 battery_power_out = []
 em_torque_out = []
@@ -83,9 +76,10 @@ alpha = []
 beta = []
 v_p = []
 v_i = []
-a_out = np.zeros(len(t_cyc))
-delta_a_out = np.zeros(len(t_cyc))
 
+v_out = np.zeros(len(t_cyc))
+a_out = np.zeros(len(t_cyc))
+em_force_w_out = np.zeros(len(t_cyc))
 
 i = 0
 for v, t in zip(v_cyc, t_cyc):
@@ -95,15 +89,22 @@ for v, t in zip(v_cyc, t_cyc):
     power_req = driver.alpha * battery.compute_max_power()
     power_req_out.append(power_req)
 
+    if power_req > 0:
+        print(power_req)
     b_out = battery.compute_step(power_req)
     battery_power_out.append(b_out['pack_power'])
 
+    if (em_gearbox.em_force_w > 0) or (battery.p_pack > 0):
+        print(em_gearbox.em_force_w, battery.p_pack)
     em.compute_step(battery.p_pack, em_gearbox.em_force_w)
     em_torque_out.append(em.em_torque)
 
     rear_brakes.compute_step(rear_wheels.wheel_torque, driver.beta)
     front_brakes.compute_step(front_wheels.wheel_torque, driver.beta)
 
+    if rear_wheels.wheel_torque > 0:
+        print(i, v_cyc[i], rear_wheels.wheel_torque)
+ 
     em_gearbox.compute_step(em.em_torque, front_brakes.front_brake_w)
 
     front_wheels.compute_step(
@@ -112,12 +113,11 @@ for v, t in zip(v_cyc, t_cyc):
     rear_wheels.compute_step(
         em_gearbox.torque_out / 2, rear_brakes.brake_torque, vehicle.velocity)
 
-    # if v_cyc[i] > 0:
-    #     print(i, v_cyc[i])
+    if v_cyc[i] > 0:
+        print(i, v_cyc[i])
     vehicle_data = vehicle.compute_step(front_wheels.force_at_wheel +
-                                        rear_wheels.force_at_wheel, 0)
+                                        rear_wheels.force_at_wheel, 0, v)
 
-    # print(v, vehicle_data['velocity'])
     front_wheel_torque_out.append(front_wheels.wheel_torque)
     rear_wheel_torque_out.append(rear_wheels.wheel_torque)
     force_at_wheel_out.append(front_wheels.force_at_wheel +
@@ -128,41 +128,22 @@ for v, t in zip(v_cyc, t_cyc):
     beta.append(driver.beta)
     v_p.append(driver_out['v_p'])
     v_i.append(driver_out['v_i'])
-    v_out_slew.append(v_slew_filter.filter(vehicle_data['velocity'], delta_t))
 
-    v_out.append(vehicle_data['velocity'])
-    a_out[i] = vehicle_data['acceleration']
-    if (v_out[i] > 0) and (i > 0):
-        dvdt = (v_out[i] - v_out[i-1])/delta_t
-        a_cyc = (v_cyc[i] - v_cyc[i-1])/delta_t
-        a_diff = (a_out[i] - dvdt)
-        delta_a_out[i] = (a_out[i] - a_cyc)
-
+    v_out[i] = vehicle_data['velocity']
+    em_force_w_out[i] = em_gearbox.em_force_w
     i += 1
-# v_delay = delay_filter(v_out_slew, a=0.01)
-v_out_lpf = first_order_filter(v_out_slew, alpha_lpf)
 
-v_out_lpf = first_order_filter(v_out_slew, alpha_lpf)
-# print(len(t_cyc), len(v_out), len(v_out_slew))
 v_cyc_mph = 2.23694*v_cyc
-v_out_slew_mph = 2.23694*np.array(v_out_slew)
-v_out_lpf_mph = 2.23694*np.array(v_out_lpf)
+v_out_mph = 2.23694*np.array(v_out)
 
 fig1, ax = plt.subplots(figsize=(11, 8.5))
-plt.plot(t_cyc, v_cyc_mph, 'b-')
-# plt.plot(t_cyc, v_cyc2, 'r--')
-# plt.plot(t_cyc, v_out, 'r--')
-plt.plot(t_cyc, v_out_slew_mph, 'r--')
-plt.grid()
-plt.plot(t_cyc, v_out_lpf_mph, 'k-.')
-plt.plot(t_cyc, v_p, 'm-.')
-plt.plot(t_cyc, v_i, 'y-.')
+plt.plot(t_cyc, v_cyc_mph, 'b--', linewidth=3.0)
+plt.plot(t_cyc, v_out_mph, 'r-')
 ax.set_title('Vehicle Speeds')
 ax.set_xlabel('Elapsed Time (s)')
 ax.set_ylabel('Vehicle Speed (mph)')
 
-plt.legend(['v_cyc', 'v_out_slew', 'v_out_lpf'], loc="upper left")
-# plt.legend(['v_cyc', 'v_out', 'v_out_slew', 'v_out_lpf'], loc="upper right")
+plt.legend(['v_cyc', 'v_out'], loc="upper left")
 
 fig2 = plt.figure()
 plt.title('battery power request')
@@ -225,16 +206,16 @@ RC_ST_V = spio.loadmat('data/RC_ST_V.mat', squeeze_me=True)
 RC_ST_V_t = RC_ST_V['t']
 RC_ST_V_in = RC_ST_V['in']
 RC_ST_V_out = RC_ST_V['out']
-dt1 = RC_ST_V_t[1] - RC_ST_V_t[0]
+dt = RC_ST_V_t[1] - RC_ST_V_t[0]
 
 RC_ST_V_out1 = np.zeros(len(RC_ST_V_out))
 for i in range(len(RC_ST_V_t)):
     t = RC_ST_V_t[i]
     icurr = RC_ST_V_in[i]
     if i == 0:
-        RC_ST_V_out1[i] = 0 # icurr * dt1
+        RC_ST_V_out1[i] = 0
     else:
-        RC_ST_V_out1[i] = RC_ST_V_out1[i-1] + icurr * dt1
+        RC_ST_V_out1[i] = RC_ST_V_out1[i-1] + icurr * dt
 
 fig1 = plt.figure(figsize=(11, 8.5))
 plt.plot(RC_ST_V_t, RC_ST_V_out, 'b-')
@@ -251,7 +232,7 @@ batt_data = spio.loadmat('data/batt_data.mat', squeeze_me=True)
 batt_data_t = batt_data['t']
 batt_data_curr_in = batt_data['curr_in']
 batt_data_soc = batt_data['soc']
-dt1 = batt_data_t[1] - batt_data_t[0]
+dt = batt_data_t[1] - batt_data_t[0]
 
 batt_soc1 = np.zeros(len(batt_data_t))
 batt_capacity_s = 378000  # 52.5Ah * 2 parallel * 3600 sec
@@ -259,9 +240,9 @@ for i in range(len(batt_data_t)):
     t = batt_data_t[i]
     icurr = - batt_data_curr_in[i]
     if i == 0:
-        batt_soc1[i] = batt_data_soc[0]  # icurr * dt1
+        batt_soc1[i] = batt_data_soc[0]
     else:
-        batt_soc1[i] = batt_soc1[i - 1] + (icurr / batt_capacity_s) * dt1
+        batt_soc1[i] = batt_soc1[i - 1] + (icurr / batt_capacity_s) * dt
 
     if batt_soc1[i] > 0.99: batt_soc1[i] = 0.99
 
@@ -281,33 +262,18 @@ veh_data_mass_kg = veh_data['mass_kg']
 veh_data_spd_mps_in = veh_data['spd_mps_in']
 vehspd_mps = veh_data['spd_mps']
 veh_data_dist_m = veh_data['dist_m']
-dt1 = veh_data_t[1] - veh_data_t[0]
+dt = veh_data_t[1] - veh_data_t[0]
 
 vehspd_mps1 = np.zeros(len(veh_data_t))
 veh_data_dist_m1 = np.zeros(len(veh_data_t))
-print('dt1 =', dt1, ' size of t_cyc =', len(veh_data_t))
+print('dt =', dt, ' size of t_cyc =', len(veh_data_t))
 
-for i in range(len(veh_data_t)):
-    a_out_i = veh_data_tract_force_N[i] / veh_data_mass_kg[i]
-    if i == 0:
-        vehspd_mps1[i] = 0
-        veh_data_dist_m1[i] = 0
-    else:
-        vehspd_mps1[i] = vehspd_mps1[i - 1] + a_out_i * dt1
-        a_delta = ((vehspd_mps[i] - vehspd_mps[i - 1]) / dt1) - a_out_i
-        v_delta = vehspd_mps1[i] - vehspd_mps[i]
-        if (vehspd_mps1[i] > 0) and (a_out_i != a_delta) and (np.abs(v_delta) > 0.01):
-            if (v_delta > 0) and (abs(a_delta) <= 0.1):
-                vehspd_mps1[i] = vehspd_mps1[i] - v_delta
-            elif (v_delta < 0) and (abs(a_delta) <= 0.1):
-                vehspd_mps1[i] = vehspd_mps1[i] + 0.1
-
-        if (vehspd_mps1[i] < 0.1): vehspd_mps1[i] = 0.0
-
-        if (vehspd_mps1[i] < 0.5) and (vehspd_mps[i] > 0.5):
-            vehspd_mps1[i] = vehspd_mps[i]
-
-        veh_data_dist_m1[i] = veh_data_dist_m1[i - 1] + vehspd_mps1[i] * dt1
+vehspd_mps1[0] = vehspd_mps[0]
+veh_data_dist_m1[0] = veh_data_dist_m[0]
+for i in range(1, len(veh_data_t)):
+    a_out = veh_data_tract_force_N[i] / veh_data_mass_kg[i]
+    vehspd_mps1[i] = Vehicle.speed_integrator(dt, a_out, vehspd_mps1[i-1], vehspd_mps[i])
+    veh_data_dist_m1[i] = veh_data_dist_m1[i - 1] + vehspd_mps1[i] * dt
 
 fig1 = plt.figure(figsize=(11, 8.5))
 plt.plot(veh_data_t, vehspd_mps, 'b-')
--- vehicle.py
+++ vehicle.py
@@ -1,11 +1,13 @@
 import numpy as np
+from first_order_filter import MinMaxSlewRateFilter
 
+v_slew = MinMaxSlewRateFilter(-2, 2)
 
 class Vehicle(object):
-    def __init__(self, mass, density_air, c_drag, frontal_area, delta_t):
+    def __init__(self, mass, density_air, c_drag, frontal_area, delta_t=0.01):
         self.mass = mass
         self.delta_t = delta_t
-        self.density_air = density_air
+        self.density_air = 1.29
         self.c_drag = c_drag
         self.frontal_area = frontal_area
         self.gravity = 9.8
@@ -15,12 +17,18 @@ class Vehicle(object):
         self.acceleration = 0
         self.position = 0
 
-    def compute_step(self, force_at_wheel, road_load):
+        self.velocity_pre = 0
+
+    def compute_step(self, force_at_wheel, road_load, v_cyc):
+        # self.v_cyc = v_cyc
         self.acceleration = (force_at_wheel - self.aero_drag) / self.mass
         self.velocity = self.velocity + self.acceleration * self.delta_t
 
         if self.velocity < 0:
             self.velocity = 0
+
+        self.velocity = self.speed_integrator(self.delta_t, self.acceleration, self.velocity_pre, v_cyc)
+        self.velocity_pre = self.velocity
 
         self.aero_drag = self.compute_areo_drag(self.velocity)
         self.position = self.position + self.velocity * self.delta_t
@@ -33,3 +41,26 @@ class Vehicle(object):
 
     def compute_areo_drag(self, velocity):
         return np.power(velocity, 2) * 0.5 * self.density_air * self.c_drag * self.frontal_area
+
+    @staticmethod 
+    def speed_integrator(delta_t, acceleration, velocity_pre, v_cyc):
+        # acceleration = (velocity - velocity_pre)/delta_t
+        vehspd_mps = velocity_pre + acceleration * delta_t
+        # vehspd_mps = np.max([0, v_slew.filter(vehspd_mps, delta_t)])
+
+        v_delta = vehspd_mps - v_cyc
+        acceleration_t = (vehspd_mps - velocity_pre)/delta_t
+        a_delta = acceleration_t - acceleration
+
+        if (vehspd_mps > 0) and (a_delta != 0) and (v_delta > 0.1):
+            if (v_delta > 0) and (abs(a_delta <= 0.01)):
+                vehspd_mps -= v_delta # 0.01
+            elif (v_delta < 0) and (abs(a_delta <= 0.01)):
+                vehspd_mps += v_delta # 0.01
+
+        if vehspd_mps < 0: vehspd_mps = 0.0
+        
+        if (vehspd_mps < 0.5) and (v_cyc < 2.0) and (acceleration_t < 0): 
+            vehspd_mps = v_cyc
+
+        return vehspd_mps
