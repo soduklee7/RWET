16671a0fca320c321f9ef6a37097aa940bc58242
soduklee7 <62019112+soduklee7@users.noreply.github.com>
2024-11-06 08:23:21 -0500

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -3,45 +3,6 @@ from first_order_filter import MinMaxSlewRateFilter
 
 v_slew = MinMaxSlewRateFilter(-2, 2)
 
-class Vehicle(object):
-    def __init__(self, mass, density_air, c_drag, frontal_area, delta_t=0.01):
-        self.mass = mass
-        self.delta_t = delta_t
-        self.density_air = 1.29
-        self.c_drag = c_drag
-        self.frontal_area = frontal_area
-        self.gravity = 9.8
-        self.aero_drag = 0
-
-        self.velocity = 0
-        self.acceleration = 0
-        self.position = 0
-
-        self.velocity_pre = 0
-
-    def compute_step(self, force_at_wheel, road_load, v_cyc):
-        # self.v_cyc = v_cyc
-        self.acceleration = (force_at_wheel - self.aero_drag) / self.mass
-        self.velocity = self.velocity + self.acceleration * self.delta_t
-
-        if self.velocity < 0:
-            self.velocity = 0
-
-        self.velocity = self.speed_integrator(self.delta_t, self.acceleration, self.velocity_pre, v_cyc)
-        self.velocity_pre = self.velocity
-
-        self.aero_drag = self.compute_areo_drag(self.velocity)
-        self.position = self.position + self.velocity * self.delta_t
-
-        return {
-            "acceleration": self.acceleration,
-            "velocity": self.velocity,
-            "position": self.position
-        }
-
-    def compute_areo_drag(self, velocity):
-        return np.power(velocity, 2) * 0.5 * self.density_air * self.c_drag * self.frontal_area
-
     @staticmethod 
     def speed_integrator(delta_t, acceleration, velocity_pre, v_cyc):
         # acceleration = (velocity - velocity_pre)/delta_t
